59f4880f83b319331965b174a81f68cb549c775d
Partha7001 <partha7001558399@gmail.com>
2026-08-18 22:05:52 +0530

    Add files via upload

---
+++ day9_Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    float p ;
+    printf("Enter percentage : ");
+    scanf("%f",&p);
+    if(p>=90){
+    printf("A ");}
+    else if
+    
+        (p>=80){
+        printf("B");
+    }
+    else if 
+        (p>=70){
+        printf("C");
+    }
+    else if 
+        (p>=60){
+        printf("D");
+    }
+    else 
+    {printf("F");}
+    return 0;
+
+}
