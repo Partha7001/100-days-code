9dd83ddef282af5c8b364add6c08006c7ce2f019
Partha7001 <partha7001558399@gmail.com>
2026-09-03 22:08:17 +0530

    Add files via upload

---
+++ day25_Q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = n; i >= 1; i--)
+    {
+        for(j = i; j <= n; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
