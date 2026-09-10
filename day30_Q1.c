572362cdfcce2ae94e0d2e99f53222b5d26eccb9
Partha7001 <partha7001558399@gmail.com>
2026-09-10 20:45:39 +0530

    Add files via upload

---
+++ day30_Q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter %d elements: ", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even = %d\n", even);
+    printf("Odd = %d", odd);
+
+    return 0;
+}
