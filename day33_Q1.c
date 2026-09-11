d5402e2d0e5cb937ac1fa5c6e9291427bfb965c1
Partha7001 <partha7001558399@gmail.com>
2026-09-11 23:54:28 +0530

    Add files via upload

---
+++ day33_Q1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, key;
+    int low, high, mid;
+    int found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter elements in sorted order: ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(a[mid] == key)
+        {
+            found = 1;
+            break;
+        }
+        else if(key < a[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    if(found)
+        printf("Element found at index %d", mid);
+    else
+        printf("Element not found");
+
+    return 0;
+}
