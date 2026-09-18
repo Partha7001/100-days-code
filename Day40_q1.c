587bf979f6d78e5dd7cfec11bc4a2012915ff197
Partha7001 <partha7001558399@gmail.com>
2026-09-18 22:39:22 +0530

    Add files via upload

---
+++ Day40_q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Diagonal traversal: ");
+
+    for (int k = 0; k < r + c - 1; k++) {
+        for (int i = 0; i < r; i++) {
+            int j = k - i;
+
+            if (j >= 0 && j < c) {
+                printf("%d ", a[i][j]);
+            }
+        }
+    }
+
+    return 0;
+}
